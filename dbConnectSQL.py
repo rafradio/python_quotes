a153349eb183e9a89ccdcde0eee461e4a0d73e2b
Abdyushev Rafail <Abdyushev.R@romir.ru>
2024-03-12 09:42:54 +0300

    dbconnect class

---
+++ dbConnectSQL.py
@@ -0,0 +1,43 @@
+import mysql.connector
+
+class DbConnectSQL:
+    def __init__(self) -> None:
+        with open("config.txt") as f:
+            rawdata = [line.rstrip() for line in f]
+        self.mydb = mysql.connector.connect(
+            host=rawdata[0],
+            user=rawdata[1],
+            password=rawdata[2],
+            database=rawdata[3]
+        )
+        self.mycursor = self.mydb.cursor()
+        self.mycursor.execute("SHOW DATABASES")
+        for x in self.mycursor:
+            print(x)
+        
+    def makeSQLInsert(self, data):
+        print(len(data))
+        sql = "INSERT INTO folkmaik_quota.quotas (link, title, prj_id, user_id) VALUES (%s, %s, %s, %s)"
+        self.mycursor.executemany(sql, data)
+        # self.mycursor.l
+        
+        # for line in data:
+        #     values = (int(line[0]), str(line[1]),int(line[2]) ,int(line[3]))
+        #     self.mycursor.execute(sql, values)
+            
+        
+        self.mydb.commit()
+        # print(self.mycursor.fetchall, "record inserted.")
+        print(self.mycursor.rowcount, "record inserted.")
+        
+    def checkSQLUsers(self, data):
+        resData = []
+        for u in data:  
+            val = "%" + str(u) + "%"
+            sql = "SELECT id FROM users WHERE email LIKE '" + val + "'"
+            self.mycursor.execute(sql)
+            row = [item[0] for item in self.mycursor.fetchall()]
+            resData.append(row)
+        print(",".join([str(x) for xs in resData for x in xs]))
+        return resData
+        
