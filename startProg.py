307ce3f4ff5fa69c10d7d8779548e2906e0ed578
Abdyushev Rafail <Abdyushev.R@romir.ru>
2024-03-12 09:44:44 +0300

    main

---
+++ startProg.py
@@ -0,0 +1,26 @@
+import sys
+import takeData
+# import dbConnectSQL
+import createDataFromFile as create
+
+def main(args):
+    projectId, users, myURL = create.createData(args[1])
+    print("start - ", projectId, users[0])
+    dt = takeData.TakeData(myURL)
+    # dBase = dbConnectSQL.DbConnectSQL()
+    
+    # dt.takeKeys()
+    dt.createData(projectId, users)
+    # dt.checkData()
+    # dt.insertData()
+    # dt.writeToFile()
+    # dt.makeSQLInsert()
+    print(dt.insertingData)
+    print(len(dt.insertingData))
+    # print(dt.mydb)
+    dt.makeSQLInsert(dt.insertingData)
+    # dBase.makeSQLInsert(dt.insertingData)
+
+if __name__ == "__main__":
+    main(sys.argv)
+    
