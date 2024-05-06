00476db54fb93063c1defe006e182e76114a0844
Abdyushev Rafail <Abdyushev.R@romir.ru>
2024-05-06 15:29:34 +0300

    check new line

--- AddressProgram.py
+++ AddressProgram.py
@@ -1,11 +1,31 @@
 import sys
 import re
+import csv
 
-def main(args):
+def checkNewLine(args):
+    data = []
+    with open('addresProgram1.csv', mode ='r') as f:
+        # csvFile = csv.reader(f, quotechar=None)
+        csvFile = csv.reader(f, delimiter  = ';', quotechar=None)
+        for lines in csvFile:
+            # print(lines)
+            data.append(lines)
+           
+    index = 1        
+    for arr in data:
+        if arr[0].rfind("@") > -1: print(arr, index)
+        index += 1
+
+def main(args):  
     with open('addresProgram.txt', encoding="utf-8") as f:
         data = [line.rstrip().replace('"', '') for line in f]
+    
+    
+    
     index = 1
     for str in data:
+        # str = stringMy[1].rstrip().replace("\\r\\n", "")
+        # str = str.replace('"', '')
         if str.rfind("@") > -1:
             allIndexes = [i for i, ltr in enumerate(str) if ltr == "@"]
             if len(allIndexes) > 1:
@@ -16,7 +36,7 @@ def main(args):
                     attempt += 1
                 
         index += 1
-        checkEmailSpelling(data)
+    checkEmailSpelling(data)
         
 def checkEmailSpelling(data):
     index = 1
@@ -37,5 +57,6 @@ def checkEmailSpelling(data):
         
 
 if __name__ == "__main__":
+    # checkNewLine(sys.argv)
     main(sys.argv)
     
