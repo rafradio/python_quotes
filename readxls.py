b3d2e7349f112319969f2ca8e8bd012aacb032f5
Abdyushev Rafail <Abdyushev.R@romir.ru>
2024-06-13 12:57:24 +0300

    clear users

---
+++ readxls.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import openpyxl
+
+dataframe = openpyxl.load_workbook("testxls.xlsm")
+
+dataframe1 = dataframe.active
+mydata = []
+dataset = []
+
+for row in range(0, dataframe1.max_row):
+    mydata.append([col[row].value for col in dataframe1.iter_cols(1, dataframe1.max_column)])
+
+for i in range(len(mydata[0])):
+    dataset.append([mydata[j][i] for j in range(len(mydata))])
+
+    
+print(mydata[4])
+print(dataset[0][4].replace("megafon\\","").rstrip() + "@megafon.ru")
+print(dataset[4][0])
+
+for i in range(len(dataset)-1):
+    checkSet = []
+    for j in range(len(dataset[i])):
+        str1 = str(dataset[i][j]).replace("megafon\\","").rstrip() + "@megafon.ru"
+        checkSet = list(filter(lambda x: str(x).find(str1) != -1, dataset[4]))
+        if len(checkSet) > 0: print(str1, checkSet)
