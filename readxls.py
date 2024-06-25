ddf807958186ebcb82be463c8af62afd1816bf25
Abdyushev Rafail <Abdyushev.R@romir.ru>
2024-06-25 10:59:10 +0300

    sql requests

--- readxls.py
+++ readxls.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import openpyxl
 import time
+import dbConnectSQL
 
 start = time.time()
 dataframe = openpyxl.load_workbook("testxls.xlsm")
+db = dbConnectSQL.DbConnectSQL("config1.txt")
 
 dataframe1 = dataframe.active
 mydata = []
@@ -15,16 +17,19 @@ for row in range(0, dataframe1.max_row):
 for i in range(len(mydata[0])):
     dataset.append([mydata[j][i] for j in range(len(mydata))])
 
-    
-# print(mydata[4])
-# print(dataset[0][4].replace("megafon\\","").rstrip() + "@megafon.ru")
-# print(dataset[4][0])
 
 for i in range(len(dataset)-1):
-    # checkSet = []
     s = set(dataset[i])
     for el in s:
         str1 = str(el).replace("megafon\\","").rstrip() + "@megafon.ru"
+        val = "%" + str(str1) + "%"
+        sql = "SELECT password FROM megafon_22.user WHERE login LIKE '" + val + "'"
+        
+        db.mycursor.execute(sql)
+        rowdata = db.mycursor.fetchall()
+        print(str1)
+        if len(rowdata) != 0:
+            print(rowdata[0])
         checkSet = list(filter(lambda x: str(x).find(str1) != -1, dataset[4]))
         if len(checkSet) > 0: print(el)
     # for j in range(len(dataset[i])):
