1b08fd4fda6958097a4d12bb03e883cee5409d6d
Abdyushev Rafail <Abdyushev.R@romir.ru>
2024-06-25 09:41:31 +0300

    with set

--- readxls.py
+++ readxls.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import openpyxl
+import time
 
+start = time.time()
 dataframe = openpyxl.load_workbook("testxls.xlsm")
 
 dataframe1 = dataframe.active
@@ -14,13 +16,21 @@ for i in range(len(mydata[0])):
     dataset.append([mydata[j][i] for j in range(len(mydata))])
 
     
-print(mydata[4])
-print(dataset[0][4].replace("megafon\\","").rstrip() + "@megafon.ru")
-print(dataset[4][0])
+# print(mydata[4])
+# print(dataset[0][4].replace("megafon\\","").rstrip() + "@megafon.ru")
+# print(dataset[4][0])
 
 for i in range(len(dataset)-1):
-    checkSet = []
-    for j in range(len(dataset[i])):
-        str1 = str(dataset[i][j]).replace("megafon\\","").rstrip() + "@megafon.ru"
+    # checkSet = []
+    s = set(dataset[i])
+    for el in s:
+        str1 = str(el).replace("megafon\\","").rstrip() + "@megafon.ru"
         checkSet = list(filter(lambda x: str(x).find(str1) != -1, dataset[4]))
-        if len(checkSet) > 0: print(str1, checkSet)
+        if len(checkSet) > 0: print(el)
+    # for j in range(len(dataset[i])):
+    #     str1 = str(dataset[i][j]).replace("megafon\\","").rstrip() + "@megafon.ru"
+    #     checkSet = list(filter(lambda x: str(x).find(str1) != -1, dataset[4]))
+    #     if len(checkSet) > 0: print(str1, j)
+        
+end = time.time()
+print("The time : ",round(end - start,5))
